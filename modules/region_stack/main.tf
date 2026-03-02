92eb460e73e3491f74586a17b09cf9b630653c3d
Ricardo Godinez <rgodinezco@gmail.com>
2026-03-02 00:44:34 -0800

    fix: region stack main

--- modules/region_stack/main.tf
+++ modules/region_stack/main.tf
@@ -3,22 +3,22 @@ locals {
 }
 
 module "hub" {
-  source              = "../hub_network"
-  location            = var.location
-  hub_cidr            = local.cfg.hub_cidr
-  bastion_subnet      = local.cfg.bastion_subnet
-  firewall_subnet     = local.cfg.firewall_subnet
-  mgmt_subnet         = local.cfg.mgmt_subnet
-  environment         = var.environment
-  tags                = var.tags
+  source         = "../hub_network"
+  location       = var.location
+  environment    = var.environment
+  hub_cidr       = local.cfg.hub_cidr
+  bastion_subnet = local.cfg.bastion_subnet
+  firewall_subnet = local.cfg.firewall_subnet
+  mgmt_subnet    = local.cfg.mgmt_subnet
+  tags           = var.tags
 }
 
 module "spoke" {
-  source              = "../spoke_network"
-  location            = var.location
-  spoke_cidr          = local.cfg.spoke_cidr
-  app_subnet          = local.cfg.app_subnet
-  pe_subnet           = local.cfg.pe_subnet
-  environment         = var.environment
-  tags                = var.tags
+  source     = "../spoke_network"
+  location   = var.location
+  spoke_cidr = local.cfg.spoke_cidr
+  app_subnet = local.cfg.app_subnet
+  pe_subnet  = local.cfg.pe_subnet
+  environment = var.environment
+  tags        = var.tags
 }
