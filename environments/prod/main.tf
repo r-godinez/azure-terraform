c9a0e5f249230bb27be574cc9bf0ba35b0623ffa
Ricardo Godinez <rgodinezco@gmail.com>
2026-03-03 00:21:47 -0800

    add: implement regions

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -1,32 +1,45 @@
 # ==================================================
-# Deploy Regions
-# ==================================================
-# module "primary_region" {
-#   source      = "../../modules/region_stack"
-#   location    = "westus3"
-#   region      = "primary"
-#   environment = "prod"
-#   region_config = var.region_config
-#   tags        = var.tags
-# }
-
-# module "secondary_region" {
-#   source      = "../../modules/region_stack"
-#   location    = "mexicocentral"
-#   region      = "secondary"
-#   environment = "prod"
-#   region_config = var.region_config
-#   tags        = var.tags
-# }
-
-# ==================================================
 # Mangament Stack
 # ==================================================
 module "management_stack" {
-  source     = "../../modules/management_stack"
+  source      = "../../modules/management_stack"
   location    = "westus3"
   environment = var.environment
   tags        = var.tags
 }
+
+# ==================================================
+# Deploy Regions
+# ==================================================
+module "primary_region" {
+  source        = "../../modules/region_stack"
+  location      = "westus3"
+  region        = "primary"
+  environment   = "prod"
+  region_config = var.region_config
+  tags          = var.tags
+}
+
+module "secondary_region" {
+  source        = "../../modules/region_stack"
+  location      = "mexicocentral"
+  region        = "secondary"
+  environment   = "prod"
+  region_config = var.region_config
+  tags          = var.tags
+}
+
+# ==================================================
+
+# ==================================================
+
+
+
+# ==================================================
+
+# ==================================================
+
+
+# ==================================================
 
 # ==================================================
--- environments/prod/variables.tf
+++ environments/prod/variables.tf
@@ -7,3 +7,15 @@ variable "tags" {
   type        = map(string)
   description = "Common resource tags"
 }
+
+variable "region_config" {
+  type = map(object({
+    hub_cidr        = string
+    spoke_cidr      = string
+    bastion_subnet  = string
+    firewall_subnet = string
+    mgmt_subnet     = string
+    app_subnet      = string
+    pe_subnet       = string
+  }))
+}
