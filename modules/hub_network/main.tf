16d20347eb7289e2f70a144cd677b35520657ee5
Ricardo Godinez <rgodinezco@gmail.com>
2026-03-03 00:21:26 -0800

    add: hub network

--- modules/hub_network/main.tf
+++ modules/hub_network/main.tf
@@ -1,9 +1,15 @@
+# Resource Group (dynamically create RG)
+resource "azurerm_resource_group" "hub_rg" {
+  name     = "rg-hub-${var.location}-${var.environment}"
+  location = var.location
+  tags     = var.tags
+}
+
 # VNet
 resource "azurerm_virtual_network" "hub" {
   name                = local.vnet_name
   location            = var.location
-  resource_group_name = "rg-mgmt-${var.location}-${var.environment}"
-
+  resource_group_name = azurerm_resource_group.hub_rg.name
   address_space = [var.hub_cidr]
 
   tags = var.tags
@@ -12,7 +18,7 @@ resource "azurerm_virtual_network" "hub" {
 # Bastion Subnet
 resource "azurerm_subnet" "bastion" {
   name                 = "AzureBastionSubnet"
-  resource_group_name  = azurerm_virtual_network.hub.resource_group_name
+  resource_group_name = azurerm_resource_group.hub_rg.name
   virtual_network_name = azurerm_virtual_network.hub.name
   address_prefixes     = [var.bastion_subnet]
 }
@@ -20,7 +26,7 @@ resource "azurerm_subnet" "bastion" {
 # Firewall Subnet
 resource "azurerm_subnet" "firewall" {
   name                 = "AzureFirewallSubnet"
-  resource_group_name  = azurerm_virtual_network.hub.resource_group_name
+  resource_group_name = azurerm_resource_group.hub_rg.name
   virtual_network_name = azurerm_virtual_network.hub.name
   address_prefixes     = [var.firewall_subnet]
 }
@@ -28,7 +34,7 @@ resource "azurerm_subnet" "firewall" {
 # Management Subnet
 resource "azurerm_subnet" "mgmt" {
   name                 = "snet-mgmt"
-  resource_group_name  = azurerm_virtual_network.hub.resource_group_name
+  resource_group_name = azurerm_resource_group.hub_rg.name
   virtual_network_name = azurerm_virtual_network.hub.name
   address_prefixes     = [var.mgmt_subnet]
 }
